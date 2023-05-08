a3bfcb700f97f44e6f63c014bf09bce85c248f99
thekylemoses <kylemoses23@gmail.com>
2023-05-08 18:18:03 -0500

    .

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,39 @@
-import Head from 'next/head';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
+import Head from 'next/head'
 
-export default function Home() {
+const HomePage = () => {
   return (
     <div className="container">
       <Head>
         <title>My Portfolio</title>
+        <meta name="description" content="My portfolio website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header title="My Portfolio" />
+      <header>
+        <div className="logo">My Portfolio</div>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
+        </nav>
+      </header>
 
       <main>
-        <section className="hero">
-          <h1>Hi, I'm Kyle Moses</h1>
-          <p>I am a computer science major and software developer. Here are some of the projects I have worked on:</p>
-        </section>
-
-        <section className="projects">
-          <h2>Projects</h2>
-          <div className="project">
-            <h3>Project 1</h3>
-            <p>Description of project 1</p>
-            <a href="#">View project</a>
-          </div>
-          <div className="project">
-            <h3>Project 2</h3>
-            <p>Description of project 2</p>
-            <a href="#">View project</a>
-          </div>
-          <div className="project">
-            <h3>Project 3</h3>
-            <p>Description of project 3</p>
-            <a href="#">View project</a>
-          </div>
-        </section>
-
-        <section className="about">
-          <h2>About Me</h2>
-          <p>I'm a computer science major at [Your University], and I'm passionate about software development and technology. In my free time, I enjoy learning new programming languages and building personal projects.</p>
-        </section>
+        <div className="hero">
+          <img src="/images/profile-picture.png" alt="My Profile Picture" className="profile-picture" />
+          <h1>Welcome to my portfolio!</h1>
+          <p>Here you can find information about me and my projects.</p>
+          <a href="/projects">View Projects</a>
+        </div>
       </main>
 
-      <Footer />
+      <footer>
+        <p>&copy; 2023 My Portfolio</p>
+      </footer>
     </div>
-  );
+  )
 }
+
+export default HomePage;
