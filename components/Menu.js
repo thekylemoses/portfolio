fc7ec6851e9bd01dce759248b819495e4220b85a
thekylemoses <kylemoses23@gmail.com>
2023-05-08 21:17:58 -0500

    Update Menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -5,7 +5,7 @@ export default function Menu() {
     <nav>
       <ul>
         <li>
-          <span><h1>KM</h1></span>
+          <span>KM</span>
         </li>
         <li>
           <Link href="/">
