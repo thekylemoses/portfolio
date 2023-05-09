b82aac92bc73d3744270ae3f831790f16db0cf13
thekylemoses <kylemoses23@gmail.com>
2023-05-08 19:22:47 -0500

    c

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import { Menu } from './menu';
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 const HomePage = () => {
   return (
