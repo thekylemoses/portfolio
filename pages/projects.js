b55ac4e04e1d7acdf2bfc7de64761306056d411a
thekylemoses <kylemoses23@gmail.com>
2023-05-08 22:28:44 -0500

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -39,12 +39,11 @@ const ProjectsPage = () => {
           <Menu />
         </div>
       </header>
-      <div className="myprojects">
-        <h1>My Projects</h1>
-      </div>
       
-
       <main>
+        <div className="myprojects">
+          <h1>My Projects</h1>
+        </div>
         <div className="projects">
           {projects.map(project => (
             <div key={project.id} className="project">
